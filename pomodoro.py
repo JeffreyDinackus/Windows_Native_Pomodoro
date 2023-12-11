04d1efeaae66a1ba2c33b74a4095ba7a61623ae3
JeffreyDinackus <jeffreydinackus@proton.me>
2023-12-11 08:15:09 -0500

    minor changes

--- pomodoro.py
+++ pomodoro.py
@@ -1,10 +1,13 @@
 from plyer import notification
 import sys
 
+
+
 import winsound
 import time
 
 # Play a beep sound
+
 
 
 # Title and message for the notification
@@ -12,8 +15,11 @@ title = 'Pomodoro time!'
 message = 'It\'s time to take a 10 min break!'
 
 
+
 title2 = 'Break Time Over'
 message2 = 'Time to get back to work!'
+
+
 
 # Sending the notification
 
@@ -27,7 +33,6 @@ while True == True:
   time.sleep(600)
   winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
   notification.notify(title=title2, message=message2, timeout=10)
-
 
 
 
